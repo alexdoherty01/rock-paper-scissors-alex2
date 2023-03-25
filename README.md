2bded6cda09f86f1e003908e11fd42440add61cb
alexdoherty01 <alexjdoherty318@gmail.com>
2023-03-25 11:50:34 +0000

    restarting browser

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # rock-paper-scissors-alex2
+
+### overview:
+- i chose rock, paper, scissors because i could demonstrate html, css, and javascript altogether.
+- game concept is straightforward but i could use enough of my javascript knowledge to demosntrate my skills
+- rock, paper, scissors demostrates the basic javascript functions.
+
+### challenges:
+- sometimes javascript did not update changes despite being functional
+- also encountered this problem when introducing a tally tracking player wins
--- assets/js/index.js
+++ assets/js/index.js
@@ -2,12 +2,14 @@ let rock = document.getElementById("rock-btn");
 let paper = document.getElementById("paper-btn");
 let scissors = document.getElementById("scissors-btn");
 let totallives = 3;
+let totalwins = 0;
 
 let options = [rock, paper, scissors]
 
 let result = document.getElementById("result");
 let playagain = document.getElementById("play-again");
 let lives = document.getElementById("lives");
+let tally = document.getElementById("tally");
 
 let playerchoice = document.getElementById("player-choice");
 let computerchoice = document.getElementById("computer-choice");
@@ -25,9 +27,13 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
         result.innerHTML = "It's a tie!";
         result.classList.add("tie");
+
     } else if (playerSelection == "rock" && computerSelection == "scissors") {
         result.innerHTML = "You win! Rock beats scissors.";
         result.classList.add("win");
+        totalwins = totalwins + 1;
+        tally.innerHTML = `${totalwins} games won`
+
     } else if (playerSelection == "rock" && computerSelection == "paper") {
         result.innerHTML = "You lose! Paper beats rock.";
         result.classList.add("lose");
@@ -37,14 +43,21 @@ function playRound(playerSelection, computerSelection) {
     } else if (playerSelection == "paper" && computerSelection == "rock") {
         result.innerHTML = "You win! Paper beats rock.";
         result.classList.add("win");
+        totalwins = totalwins + 1;
+        tally.innerHTML = `${totalwins} games won`
+
     } else if (playerSelection == "paper" && computerSelection == "scissors") {
         result.innerHTML = "You lose! Scissors beats paper.";
         result.classList.add("lose");
         totallives = totallives - 1;
         lives.innerHTML = `${totallives} lives remaining`
+
     } else if (playerSelection == "scissors" && computerSelection == "paper") {
         result.innerHTML = "You win! Scissors beats paper.";
         result.classList.add("win");
+        totalwins = totalwins + 1;
+        tally.innerHTML = `${totalwins} games won`
+        
     } else if (playerSelection == "scissors" && computerSelection == "rock") {
         result.innerHTML = "You lose! Rock beats scissors.";
         result.classList.add("lose");
